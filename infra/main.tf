ed0be18dd69cb0f6e12f464b2f0208536256ea2f
Tyler Geiger <tg996676@wcupa.edu>
2025-09-15 17:45:42 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,30 +2,26 @@ resource "cloudlab_portal_experiment" "CC2-Cluster" {
   name            = "CampusConnect"
   wait_for_status = "ready"
 
-  xenvm {
-    name           = "vm1"
-    cores          = 2
-    ram_mb         = 4096
-    disk_gb        = 20
-    routable_ip    = true
+  rawpc {
+    name          = "vm1"
+    hardware_type = "d430"
+    exclusive     = true
+    routable_ip   = true
   }
 
-  xenvm {
-    name           = "vm2"
-    cores          = 2
-    ram_mb         = 4096
-    disk_gb        = 20
-    routable_ip    = true
+  rawpc {
+    name          = "vm2"
+    hardware_type = "d430"
+    exclusive     = true
+    routable_ip   = true
   }
 
-  xenvm {
-    name           = "vm3"
-    cores          = 2
-    ram_mb         = 4096
-    disk_gb        = 20
-    routable_ip    = true
+  rawpc {
+    name          = "vm3"
+    hardware_type = "d430"
+    exclusive     = true
+    routable_ip   = true
   }
-
   lan {
     name = "lan0"
     interface { node = "vm1" }
